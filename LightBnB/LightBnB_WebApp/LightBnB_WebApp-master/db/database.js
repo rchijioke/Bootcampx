effbb1b7cc234704c76ed4ecf58e588c41271488
Roy <ekeneroy@gmail.com>
2023-06-04 13:26:45 -0500

    LighthouseBnB Reservations

--- LightBnB/LightBnB_WebApp/LightBnB_WebApp-master/db/database.js
+++ LightBnB/LightBnB_WebApp/LightBnB_WebApp-master/db/database.js
@@ -1,17 +1,18 @@
 const properties = require("./json/properties.json");
 const users = require("./json/users.json");
-const { Pool } = require('pg');
-
+const { Pool } = require("pg");
 
 const pool = new Pool({
-  user: 'labber',
-  password: '123',
-  host: 'localhost',
-  database: 'lightbnb'
+  user: "labber",
+  password: "123",
+  host: "localhost",
+  database: "lightbnb",
 });
 
 // the following assumes that you named your connection variable `pool`
-pool.query(`SELECT title FROM properties LIMIT 10;`).then(response => {console.log(response)})
+pool.query(`SELECT title FROM properties LIMIT 10;`).then((response) => {
+  console.log(response);
+});
 
 /**
  * Get a single user from the database given their email.
@@ -19,7 +20,6 @@ pool.query(`SELECT title FROM properties LIMIT 10;`).then(response => {console.l
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithEmail = function (email) {
-
   const query = `
   SELECT *
   FROM users
@@ -28,18 +28,18 @@ const getUserWithEmail = function (email) {
   `;
   const values = [email.toLowerCase()];
 
-  return pool.query(query, values)
-  .then((result) => {
-    if (result.rows.length === 0) {
-      return null; // User does not exist
-    }
-    return result.rows[0]; // Return the user object
-
-  })
-  .catch((err) => {
-    console.error('Error executing query', err);
-  });
-}
+  return pool
+    .query(query, values)
+    .then((result) => {
+      if (result.rows.length === 0) {
+        return null; // User does not exist
+      }
+      return result.rows[0]; // Return the user object
+    })
+    .catch((err) => {
+      console.error("Error executing query", err);
+    });
+};
 
 /**
  * Get a single user from the database given their id.
@@ -48,14 +48,14 @@ const getUserWithEmail = function (email) {
  */
 const getUserWithId = function (id) {
   return pool
-  .query('SELECT * FROM users WHERE id = $1', [id])
-  .then((result) => {
-    return result.rows[0] || null;
-  })
-  .catch((err) => {
-    console.error('Error executing query', err);
-    throw err;
-  });
+    .query("SELECT * FROM users WHERE id = $1", [id])
+    .then((result) => {
+      return result.rows[0] || null;
+    })
+    .catch((err) => {
+      console.error("Error executing query", err);
+      throw err;
+    });
 };
 
 /**
@@ -66,14 +66,14 @@ const getUserWithId = function (id) {
 const addUser = function (user) {
   return pool
     .query(
-      'INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING *',
+      "INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING *",
       [user.name, user.email, user.password]
     )
     .then((result) => {
       return result.rows[0];
     })
     .catch((err) => {
-      console.error('Error executing query', err);
+      console.error("Error executing query", err);
       throw err;
     });
 };
@@ -85,7 +85,24 @@ const addUser = function (user) {
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function (guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  const query = `
+    SELECT reservations.*, properties.*, AVG(property_reviews.rating) AS average_rating
+    FROM reservations
+    JOIN properties ON reservations.property_id = properties.id
+    JOIN property_reviews ON properties.id = property_reviews.property_id
+    WHERE reservations.guest_id = $1
+    GROUP BY reservations.id, properties.id
+    ORDER BY reservations.start_date
+    LIMIT 10;
+  `;
+
+  return pool
+    .query(query, [guest_id])
+    .then((result) => result.rows)
+    .catch((err) => {
+      console.error('Error executing query:', err);
+      throw err;
+    });
 };
 
 /// Properties
@@ -101,7 +118,7 @@ const getAllProperties = (options, limit = 10) => {
     .query(`SELECT * FROM properties LIMIT $1`, [limit])
     .then((result) => {
       console.log(result.rows);
-      return result.rows
+      return result.rows;
     })
     .catch((err) => {
       console.log(err.message);
